7c0264a9e13cd6331a5002dbe212352212cc5e4e
Hrithik <hrithikbudhiraja@gmail.com>
2023-04-26 00:53:54 +0530

    Data Transfromation

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 import sys
 from src.logger import logging
 
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -11,7 +12,7 @@ def error_message_detail(error, error_detail: sys):
 
 class customException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(
             error_message, error_detail=error_detail)
 
